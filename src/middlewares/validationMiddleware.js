394fc04ff40c786746277c9ee7c38d415571b974
LisandroArpi <lisandroarpi@hotmail.com>
2020-04-22 01:50:32 -0300

    validacion de datos

--- src/middlewares/validationMiddleware.js
+++ src/middlewares/validationMiddleware.js
@@ -1,8 +1,13 @@
 const Joi = require("@hapi/joi");
 
 const schemaBody = Joi.object({
-    nombre: Joi.string().alphanum().min(3).max(15).required(),
-    edad: Joi.number().optional()
+    nombre: Joi.string().alphanum().min(3).max(15).required().messages({
+        'string.base': `"nombre" debe ser un 'texto'`, 
+        'string.empty': '"nombre" no puede estar vacio'
+    }),
+    edad: Joi.number().optional(),
+    email: Joi.string().email().required(),
+    año: Joi.number().integer().min(1900).max(2020).required()
 });
 
 async function validateBody(req, res, next) {
@@ -11,7 +16,9 @@ async function validateBody(req, res, next) {
         next();
     }
     catch (err) {
-        res.status(400).json(err)
+        res.status(400).json({
+            "mensaje": err.details[0].message
+        })
     }
 }
 
